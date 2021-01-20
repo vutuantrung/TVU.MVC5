d99f2e673da840f7e32e7123ae04dac094a80676
VUTuanTrung <trung.lespace@gmail.com>
2021-01-20 11:44:18 +0100

    Create ASP .NET Framework Web application MVC

---
+++ MVCProject/Vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes( RouteCollection routes )
+        {
+            routes.IgnoreRoute( "{resource}.axd/{*pathInfo}" );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}", // Ex: movies/edit/1 => MoviesController and Edit(int id)
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }// If the url is not like the right format, it will pass to the HomeController, Index()
+            );
+        }
+    }
+}
