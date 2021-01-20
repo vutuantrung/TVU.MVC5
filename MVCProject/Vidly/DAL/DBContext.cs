165cb9f02bc8075226b0bb716afb9a6e1beaf096
VU Tuan Trung <trung.lespace@gmail.com>
2021-01-20 15:57:45 +0100

    install entity frameworks, add DBContext, Enable-Migrations

---
+++ MVCProject/Vidly/DAL/DBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DAL
+{
+    public class DBContext : DbContext
+    {
+        public DBContext()
+            : base( ConfigurationManager.ConnectionStrings[ "vidlyConnectionString" ].ConnectionString )
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
